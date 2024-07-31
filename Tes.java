0c4e399b2c0ec708593d53cd2dbed48183ef876d
raffixixi <muhammad_raffi_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 13:53:01 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,7 @@
+public class Tes {
+    public static void main(String[] args) {
+        String nama = "Raffi";
+        String kelas = "X RPL 5";
+        System.out.print(" Halo, nama saya" + nama + " dari kelas " + kelas);
+    }
+}
